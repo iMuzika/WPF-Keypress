cff9d121a05066db5f860778f9a80d765495e7c6
iMuzika <iMuzika@icloud.com>
2022-07-16 14:45:37 +0800

    Add project files.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Text.RegularExpressions
+Class MainWindow
+    Private Sub UpperCaseAlphabet(ByVal sender As Object, ByVal e As TextCompositionEventArgs)
+        Dim regex As Regex = New Regex("[^A-Z]+")
+        e.Handled = regex.IsMatch(e.Text)
+    End Sub
+    Private Sub LowerCaseAlphabet(ByVal sender As Object, ByVal e As TextCompositionEventArgs)
+        Dim regex As Regex = New Regex("[^a-z]+")
+        e.Handled = regex.IsMatch(e.Text)
+    End Sub
+    Private Sub NumberHypenPlus(ByVal sender As Object, ByVal e As TextCompositionEventArgs)
+        Dim regex As Regex = New Regex("[^0-9-+]+")
+        e.Handled = regex.IsMatch(e.Text)
+    End Sub
+End Class
